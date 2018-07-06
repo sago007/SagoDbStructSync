c2571650adb90d3c20486ae7d071f48ec0467fc4
Poul Sander <poul@poulsander.com>
2018-07-06 18:09:35 +0200

    Add License information and note about MySQL license

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SagoDbStructSync
-Program designed to syncronize database model/structure between databases
+Program designed to synchronize database model/structure between databases
+
+
+# License
+The source is under the MIT license. Be aware that the final product is under the GPLv2 if compiled against the MySQL library.
--- test/basictest.cpp
+++ test/basictest.cpp
@@ -1,3 +1,29 @@
+/*
+  Its is under the MIT license, to encourage reuse by cut-and-paste.
+
+  Copyright (c) 2018 Poul Sander
+
+  Permission is hereby granted, free of charge, to any person
+  obtaining a copy of this software and associated documentation files
+  (the "Software"), to deal in the Software without restriction,
+  including without limitation the rights to use, copy, modify, merge,
+  publish, distribute, sublicense, and/or sell copies of the Software,
+  and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 #define BOOST_TEST_MAIN
 #if !defined( WIN32 )
 #define BOOST_TEST_DYN_LINK
@@ -8,7 +34,7 @@
 #include <cereal/archives/json.hpp>
 
 /*
- This test just ensures that the thing actually compiles. 
+ This test just ensures that the thing actually compiles.
  */
 BOOST_AUTO_TEST_CASE(It_compiles) {
 	sago::database::DbTable t;
